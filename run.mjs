2ca78079d25fe1b7518d05ddc3f5da330cb31424
megahomyak <g.megahomyak@gmail.com>
2025-06-17 18:26:08 +0500

    "ensure" is tiring me

--- run.mjs
+++ run.mjs
@@ -1,4 +1,5 @@
 import { ElevenLabsClient } from "@elevenlabs/elevenlabs-js";
+import Groq from "groq-sdk";
 import * as fs from "node:fs/promises";
 import * as process from "node:process";
 
@@ -7,20 +8,51 @@ let config = JSON.parse(await fs.readFile("config.json", { encoding: "utf-8" }))
 let elevenLabsClient = new ElevenLabsClient({
     apiKey: config["eleven_labs_api_key"],
 });
+let groqClient = new Groq({
+    apiKey: config["groq_api_key"],
+});
 
-let ensure = async (fileName, defaultMaker) => {
-    let exists = false;
+let ensure = async (fileName, options, defaultMaker) => await (async function ensure() {
     try {
-        await fs.stat(fileName);
-        exists = true;
-    } catch {}
-    if (!exists) {
-        await fs.writeFile(fileName, await defaultMaker());
+        return await fs.readFile(fileName, options);
+    } catch {
+        let newContents = await defaultMaker();
+        await fs.writeFile(fileName, newContents, options);
+        return await ensure();
     }
-    return fileName;
+})();
+let read = async (fileName, options, defaultMaker) => {
+    await ensure(fileName, options, defaultMaker);
 };
+let script = ensure("script", { encoding: "utf-8" }, async () => {
+    let setting = await fs.readFile("setting", { encoding: "utf-8" });
+    return (await groqClient.chat.completions.create({
+        messages: [
+            {
+                role: "system",
+                content: "The user will provide a TODO !!!!!!!!",
+            },
+            {
+                role: "user",
+                content: setting,
+            },
+        ],
+        model: "llama-3.3-70b-versatile",
+    })).choices[0].message.content;
+}).trim();
+for (let line of script.split("\n")) {
+    let handlers = [
+        [/^say\s+(?<stereoBalance>[+-]?\d*\.?\d+)\s+(?<phrase>.+)$/, groups => {
+            ensure()
+        }]
+    ];
+    let sayMatch = line.match();
+    if (sayMatch != null) {
 
-ensure("script", )
+    } else {
+
+    }
+}
 
 let soxParams = [];
 fs.readFile("script", { encoding: "utf-8" });
