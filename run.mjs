ea55bfbcf1be8170f78c6ee1b2ba5374ac238c04
megahomyak <g.megahomyak@gmail.com>
2025-06-17 19:01:40 +0500

    If I can do the voice gen, this is finished. 96 lines of pure bullshit (dumbass)

--- run.mjs
+++ run.mjs
@@ -1,7 +1,7 @@
 import { ElevenLabsClient } from "@elevenlabs/elevenlabs-js";
 import Groq from "groq-sdk";
 import * as fs from "node:fs/promises";
-import * as process from "node:process";
+import * as childProcess from "node:child_process";
 
 let config = JSON.parse(await fs.readFile("config.json", { encoding: "utf-8" }));
 
@@ -12,25 +12,42 @@ let groqClient = new Groq({
     apiKey: config["groq_api_key"],
 });
 
-let ensure = async (fileName, options, defaultMaker) => await (async function ensure() {
+let ensure = async (filePath, defaultMaker) => {
+    let exists = false;
     try {
-        return await fs.readFile(fileName, options);
-    } catch {
-        let newContents = await defaultMaker();
-        await fs.writeFile(fileName, newContents, options);
-        return await ensure();
+        await fs.stat(filePath);
+        exists = true;
+    } catch {}
+    if (!exists) {
+        await defaultMaker(filePath);
     }
-})();
-let read = async (fileName, options, defaultMaker) => {
-    await ensure(fileName, options, defaultMaker);
+    return filePath;
 };
-let script = ensure("script", { encoding: "utf-8" }, async () => {
+let read = async (filePath, options, defaultMaker) => {
+    await ensure(filePath, async path => {
+        await fs.writeFile(path, await defaultMaker(), options);
+    });
+    return await fs.readFile(filePath, options);
+};
+let spawn = (command, args) => new Promise(resolve => childProcess.spawn(command, args).on("exit", resolve));
+
+let script = read("script", { encoding: "utf-8" }, async () => {
     let setting = await fs.readFile("setting", { encoding: "utf-8" });
     return (await groqClient.chat.completions.create({
         messages: [
             {
                 role: "system",
-                content: "The user will provide a TODO !!!!!!!!",
+                content: "The user will provide a setting for a one-sided roleplay document; please, respond ONLY WITH text in the following format:\n"
+                + "\n"
+                + "say <phrase>\n"
+                + "wait <number of seconds, real>\n"
+                + "play <sound desc>\n"
+                + "\n"
+                + "EXAMPLE:\n"
+                + "say finally, it's time to do the maintenance in the extremely dangerous metal-pipes-falling-on-workers'-feet-area!\n"
+                + "wait 2.5\n"
+                + "play metal pipe loudly falling on the floor\n"
+                + "say AW FUCK, MY FOOT!",
             },
             {
                 role: "user",
@@ -40,44 +57,40 @@ let script = ensure("script", { encoding: "utf-8" }, async () => {
         model: "llama-3.3-70b-versatile",
     })).choices[0].message.content;
 }).trim();
+let soxParams = [];
 for (let line of script.split("\n")) {
     let handlers = [
-        [/^say\s+(?<stereoBalance>[+-]?\d*\.?\d+)\s+(?<phrase>.+)$/, groups => {
-            ensure()
-        }]
+        [/^say\s+(?<phrase>.+)$/, async groups => {
+            await fs.mkdir("sayings", { recursive: true });
+            return await ensure("sayings/" + Buffer.from(groups.phrase).toString("base64url"), async path => {
+                // TODO
+            });
+        }],
+        [/^wait\s+(?<timeSeconds>.+)$/, async groups => {
+            await fs.mkdir("waits", { recursive: true });
+            return await ensure("waits/" + Buffer.from(groups.timeSeconds).toString("base64url"), async path => {
+                await spawn("sox", `-n -r 48000 ${path} trim 0.0 ${groups.timeSeconds}`.split());
+            });
+        }],
+        [/^play\s+(?<soundDescription>.+)$/, async groups => {
+            await fs.mkdir("sounds", { recursive: true });
+            return await ensure("sounds/" + Buffer.from(groups.soundDescription).toString("base64url"), async path => {
+                await fs.writeFile(
+                    path,
+                    await elevenLabsClient.textToSoundEffects.convert({ text: groups.soundDescription, promptInfluence: 1, outputFormat: "opus_48000_192" }), // Best-fidelity option: "pcm_24000". Using OPUS because at least I can listen to it through my audio players, and it also has metadata embedded which is really nice, PCM doesn't even save its own bit rate or anything else, I don't think it even has the magic file type bytes (what I call a "prefix")
+                );
+            });
+        }],
     ];
-    let sayMatch = line.match();
-    if (sayMatch != null) {
-
-    } else {
-
-    }
+    await (async () => {
+        for (let [regex, handler] in handlers) {
+            let match = line.match(regex);
+            if (match != null) {
+                soxParams.push(await handler(match.groups));
+                return;
+            }
+        }
+        throw new Error(`WTF?line: ${line}`);
+    })();
 }
-
-let soxParams = [];
-fs.readFile("script", { encoding: "utf-8" });
-process.execve("sox", soxParams);
-
-let read = async (path, defaultGenerator) => {
-    try {
-        return await fs.readFile(path, { encoding: "utf-8" });
-    } catch {
-        let newContents = await defaultGenerator();
-        await fs.writeFile(path, newContents, { encoding: "utf-8" });
-        return newContents;
-    }
-};
-
-    await elevenLabsClient.textToSpeech.convert(await read("voice_id", async () => {
-
-    }));
-
-let getSound = async soundDescription => {
-    let soundFileName = Buffer.from(soundDescription).toString("base64url");
-    return await read(soundFileName, async () => {
-
-    });
-await fs.writeFile(
-    await elevenLabsClient.textToSoundEffects.convert({ text: "metal pipe falling on the floor", promptInfluence: 1, outputFormat: "opus_48000_192" }), // Best-fidelity option: "pcm_24000". Using OPUS because at least I can listen to it through my audio players, and it also has metadata embedded which is really nice, PCM doesn't even save its own bit rate or anything else, I don't think it even has the magic file type bytes (what I call a "prefix")
-);
-};
+await spawn("sox", [...soxParams, "outfile.wav"]);
