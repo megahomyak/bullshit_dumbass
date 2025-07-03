931d52595069a81dae0ed58972345a4b5543fbde
megahomyak <g.megahomyak@gmail.com>
2025-07-03 22:47:58 +0500

    Trying to debug this stuff, fixing bugs in the process

--- redoing.mjs
+++ redoing.mjs
@@ -5,11 +5,13 @@ import * as crypto from "node:crypto";
 import Groq from "groq-sdk";
 import { ElevenLabsClient } from "@elevenlabs/elevenlabs-js";
 
-// Works like "GNU make", but only checks for the existence of the file: runs "maker" if the file by filepath doesn't exist. Returns nothing. Example: await make("myfile.txt", async filepath => await fs.writeFile(filepath, "Hello, world!"))
+// Works like "GNU make", but only checks for the existence of the file: runs "maker" if the file by filepath doesn't exist. Returns nothing. Logs file presence. Example: await make("myfile.txt", async filepath => await fs.writeFile(filepath, "Hello, world!"))
 let make = async (filepath, maker) => {
     try {
         await fsPromises.stat(filepath);
+        console.log(`${filepath} present`);
     } catch {
+        console.log(`${filepath} not present, making...`);
         await maker(filepath);
     }
     return filepath;
@@ -17,34 +19,42 @@ let make = async (filepath, maker) => {
 
 // Executes the provided command with the provided args, throws an error if the execution fails, returns the contents of the standard output as a Buffer. Example: await exec("ls", ["-l"])
 let exec = async (command, args) => new Promise((resolve, reject) => {
-    let process = childProcess.spawn(command, args);
-    let stdout = new Buffer();
-    process.stdout.on("data", data => stdout += data);
+    console.log(command + " " + args.join(" "));
+    let process = childProcess.spawn(command, args, {
+        stdio: [
+            "ignore",
+            "pipe",
+            "inherit",
+        ],
+    });
+    let stdoutChunks = [];
+    process.stdout.on("data", chunk => stdoutChunks.push(chunk));
     process.on("close", code => {
-        if (code == 0) { resolve(stdout); }
+        if (code == 0) { resolve(Buffer.concat(stdoutChunks)); }
         else { reject(new Error(`exited with ${code}`)); }
     });
 });
 
 // Compiles a list of input audio files into one audio file using ffmpeg. The outfile must be OPUS. Returns nothing. Example: await compile([{ start: 3.3/*seconds*/, pan: -1/*from -1 for "all to left" to 1 for "all to right"*/, path: "in1.mp3", duration: 5.6/*seconds*/ }, { start: 0, duration: 8, pan: 0, path: "in2.opus" }], 15.86/*seconds*/, "outfile.opus")
-let compile = async (infiles, outduration, outfilepath) => await exec("ffmpeg", [
+let compile = async (infiles, outduration, outfilepath) => console.log(infiles) || await exec("ffmpeg", [
     "-f", "lavfi",
     "-i", `anullsrc=cl=stereo:sample_rate=48000:d=${outduration}`,
     ...infiles.flatMap(infile => ["-i", infile.path]),
     "-filter_complex", (() => {
         let balanceLeft = n => 1 - Math.max(0, n);
-        let outputLabels = "";
+        let outputLabels = [];
         let filterComplex = "";
         infiles.forEach((infile, i) => {
+            i += 1;
             let outputLabel = `[processed${i}]`;
             filterComplex += `[${i}]`;
             filterComplex += `adelay=${infile.start}s:all=1`;
             filterComplex += `,pan=stereo|c0=${balanceLeft(infile.pan)}*c0|c1=${balanceLeft(-infile.pan)}*c1`;
             filterComplex += `,atrim=end=${infile.duration}`;
             filterComplex += outputLabel + ";";
-            outputLabels += outputLabel;
+            outputLabels.push(outputLabel);
         });
-        return filterComplex + `${outputLabels}amix=inputs=${outputLabels.length + 1}:duration=first[out]`;
+        return filterComplex + `[0]${outputLabels.join("")}amix=inputs=${outputLabels.length + 1}:duration=first[out]`;
     })(),
     "-map", "[out]",
     "-c:a", "libopus",
@@ -54,7 +64,7 @@ let compile = async (infiles, outduration, outfilepath) => await exec("ffmpeg", 
 
 // Gets the duration of the audio file by filepath, in seconds floating point. Can handle OPUS and MP3. Example: await getDuration("a/b/sound.mp3")
 let getDuration = async filepath => {
-    let durationBuffer = await exec("ffprobe", ["-v", "error", "-show_entries", "format=duration", "-of", "default=noprint_wrappers=1:nokey=1", filepath]);
+    let durationBuffer = (await exec("ffprobe", ["-v", "error", "-show_entries", "format=duration", "-of", "default=noprint_wrappers=1:nokey=1", filepath]));
     return parseFloat(durationBuffer.toString("utf-8"));
 };
 
@@ -64,10 +74,10 @@ let makeTextCompleter = (groqSettings) => {
         apiKey: groqSettings.apiKey,
     });
     // Completes the input messages using a chat-oriented Large Language Model, returns the response of the model as a string. Example: await completer([{ role: "system", content: "Only respond with one number." }, { role: "user", content: "What's 2 + 2?" }, { role: "assistant", content: "4" }, { role: "user", content: "And 5 + 5?" }])
-    return async messages => await groqClient.chat.completions.create({
+    return async messages => (await groqClient.chat.completions.create({
         messages,
         model: groqSettings.model,
-    });
+    })).choices[0].message.content;
 };
 
 // See the returned closure for the documentation
@@ -142,22 +152,22 @@ let base = basePath => ({
     file: name => path.join(basePath, name),
 });
 
-// A helper routine to match a command, throwing an exception if none of the matchers worked for the command, invoking the respective handler if a matcher did work. Example: await matchCommand(userCommand, [["add number number", async (a, b) => a + b], ["subtract number number", async (a, b) => a - b]]) . Supported types: "number" (\d*.?\d+), "rest" (.+). The numbers get automatically converted to type "number"
+// A helper routine to match a command, throwing an exception if none of the matchers worked for the command, invoking the respective handler if a matcher did work. Example: await matchCommand(userCommand, [["add number number", async (a, b) => a + b], ["subtract number number", async (a, b) => a - b]]) . Supported types: "number" (-?\d*.?\d+), "rest" (.+). The numbers get automatically converted to type "number"
 let matchCommand = async (command, matchers) => {
     for (let matcher of matchers) {
         let [filter, handler] = matcher;
         let [commandName, ...args] = filter.split(" ");
         let argsMatched = args.map(a => ({
             number: {
-                regexp: "\\d*.?\\d+",
+                regexp: "-?\\d*.?\\d+",
                 mapper: a => parseFloat(a),
             },
             rest: {
-                regexp: "\\s+",
+                regexp: ".+",
                 mapper: a => a,
             },
         }[a]));
-        let argsString = argsMatched.map(a => `\\s+(${a.regexp})`);
+        let argsString = argsMatched.map(a => `\\s+(${a.regexp})`).join("");
         let match = new RegExp(`^${commandName}${argsString}$`).exec(command);
         if (match != null) {
             let argValuesStrings = match.slice(1);
@@ -212,7 +222,7 @@ let makeFileName = string => {
         await fsPromises.writeFile(filepath, await textCompleter([
             {
                 role: "system",
-                content: "Please, write out a story as the user requests. The story must be calm and slowly paced, as if the reader is tired. The story should be entirely unidirectional: there must be no interactions from the reader except implied movement (for example, following someone somewhere *per their request*), but no communication from the reader should occur, verbal or non-verbal. You can insert various actions, movement, sounds or other non-verbal details.",
+                content: "Please, write out a story as the user requests. The story must be calm and slowly paced, as if the reader is tired. The story should be entirely unidirectional: there must be no interactions from the reader except implied movement (for example, following someone somewhere *per their request*), but no communication from the reader should occur, verbal or non-verbal. You can insert various actions, movement, sounds or other non-verbal details. The story must be very objective, with no descriptions of subjective internal feelings or some abstract descriptions.",
             },
             {
                 role: "user",
@@ -227,9 +237,9 @@ let makeFileName = string => {
                 role: "system",
                 content: [
                     "Turn the story sent by the user into a document of the following format: on each line, there should be one of the following commands:",
-                    "* voice {panning, from -1 for \"all to left\" to 1 for \"all to right\"} {phrase} - starts a voice from the character, goes to later commands only after the voice ends",
-                    "* sound {panning, from -1 for \"all to left\" to 1 for \"all to right\"} {duration in seconds} {sound description} - starts a sound, goes to later commands only after the sound ends",
-                    "* bgstart {panning, from -1 for \"all to left\" to 1 for \"all to right\"} {sound description} - starts playing a background sound, goes to later commands immediately",
+                    "* voice {panning, from -1 for \"all to left\" to 1 for \"all to right\"} {phrase} - starts a voice saying the phrase from the character, NOT FROM THE NARRATOR (you MUST NOT voice the narrator, everything should be expressed as if it just happens without any narration), MUST be only literal, goes to later commands only after the voice ends",
+                    "* sound {panning, from -1 for \"all to left\" to 1 for \"all to right\"} {duration in seconds} {sound description} - starts the specified sound, MUST have a literal description, goes to later commands only after the sound ends",
+                    "* bgstart {panning, from -1 for \"all to left\" to 1 for \"all to right\"} {sound description} - starts playing the specified background sound, MUST have a literal description, goes to later commands immediately",
                     "* bgstop {sound description} - stops playing a previously mentioned background sound, goes to later commands immediately",
                     "* wait {time in seconds, floating point} - waits the specified amount of time before going to later commands",
                     "",
@@ -264,31 +274,32 @@ let makeFileName = string => {
                 let endTime = currentTime;
                 let totalDuration = endTime - startTime;
                 let infilesShort = [];
+                console.log("AAAAAAAAAAAAAAAAAAAAAA", totalDuration, oneDuration);
                 new Array(Math.floor(totalDuration / oneDuration)).forEach((_, i) => {
                     infilesShort.push({
                         duration: oneDuration,
-                        start: i * oneDuration,
+                        relativeStart: i * oneDuration,
                     });
                 });
                 let remainderDuration = totalDuration % oneDuration;
                 if (remainderDuration != 0) {
                     infilesShort.push({
                         duration: remainderDuration,
-                        start: infilesShort.length * oneDuration,
+                        relativeStart: infilesShort.length * oneDuration,
                     });
                 }
                 infiles.push(...infilesShort.map(infileShort => ({
                     path: filepath,
                     pan: panning,
-                    start: infileShort.start,
+                    start: startTime + infileShort.relativeStart,
                     duration: infileShort.duration,
                 })));
             };
         };
         for (let line of formattedStory.trim().split("\n")) {
-            matchCommand(line, [
+            await matchCommand(line, [
                 ["voice number rest", async (panning, phrase) => {
-                    let filepath = await make(voices.file(makeFileName(phrase)), async filepath => {
+                    let filepath = await make(voices.file(makeFileName(phrase) + ".mp3"), async filepath => {
                         await voiceMaker(phrase, filepath);
                     });
                     let duration = await getDuration(filepath);
@@ -301,7 +312,7 @@ let makeFileName = string => {
                     currentTime += duration;
                 }],
                 ["sound number number rest", async (panning, duration, soundDescription) => {
-                    let filepath = await make(sounds.file(makeFileName(soundDescription)), async filepath => {
+                    let filepath = await make(sounds.file(makeFileName(soundDescription) + ".opus"), async filepath => {
                         await soundMaker(soundDescription, duration, filepath);
                     });
                     infiles.push({
@@ -313,7 +324,7 @@ let makeFileName = string => {
                     currentTime += duration;
                 }],
                 ["bgstart number rest", async (panning, soundDescription) => {
-                    let filepath = await make(sounds.file(makeFileName(soundDescription)), async filepath => {
+                    let filepath = await make(sounds.file(makeFileName(soundDescription) + ".opus"), async filepath => {
                         let duration = null;
                         await soundMaker(soundDescription, duration, filepath);
                     });
